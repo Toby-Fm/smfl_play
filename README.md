c812575d8e55c01fa017f4b2e948851eba92831d
Toby-Fm <mail@toby-wichmann.de>
2024-05-28 21:39:14 +0200

    add Tahoma Font

--- README.md
+++ README.md
@@ -1,22 +1,3 @@
 # smfl_linux
 
-Play Around with smfl on Linux (Ubuntu)
-
-## What is this?
-
-Ein simples Programm, was ein Fenster erstellt und einen Kreis in die Mitte zeichnet.
-<img src="./img/cpp_smfl_linux_ubuntu.png">
-
-## Use
-
-
-```bash
-git clone https://github.com/Toby-Fm/smfl_linux_template.git
-cd smfl_linux_template
-```
-
-```bash
-make linux-install
-make
-./main
-```
+Playing around with smfl on linux
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 
 int main() {
+    // Erstelle ein Fentser
+    
     sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, World!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Erstelle einen Kreis
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Magenta);
     
     // Setze den Kreis in die Mitte des Fensters
     sf::Vector2u windowSize = window.getSize();
@@ -12,7 +15,24 @@ int main() {
     shapePosition.x = windowSize.x / 2 - shape.getRadius();
     shapePosition.y = windowSize.y / 2 - shape.getRadius();
     shape.setPosition(shapePosition);
+    
+    // Erstelle eine Schrift
+    sf::Font font;
+    if (!font.loadFromFile("assets/fonts/tahoma.ttf"))
+    {
+        std::cerr << "Konnte Font nicht laden" << std::endl;
+        return 1;
+    }
 
+    // Text schreiben
+    sf::Text text;
+    text.setString("Hello, World!"); // Setze den Text
+    text.setCharacterSize(24); // Setze die Schriftgröße
+    text.setFillColor(sf::Color::Red); // Setze die Schriftfarbe
+    text.setFont(font); // Setze die Schriftart
+    text.setStyle(sf::Text::Underlined);
+
+    // Schleife, die so lange läuft, bis das Fenster geschlossen wird
     while (window.isOpen()) 
     {
         sf::Event event;
@@ -23,8 +43,10 @@ int main() {
                 window.close();
             }
         }
-
-        window.clear();
+        
+        // Clear the window with a black color
+        window.clear(sf::Color::Black);
+        window.draw(text);
         window.draw(shape);
         window.display();
     }
